8d3402d95d85168560e6163b8ff8a3112dd3b76e
Paul Gilbertson <paul.gilbertson@gmail.com>
2017-03-07 14:26:59 +0000

    Nunjucks framework setup

--- app/server.js
+++ app/server.js
@@ -1,11 +1,19 @@
-var express = require("express");
+var express = require('express');
+var nunjucks = require('nunjucks');
 
 var app = express();
 
+nunjucks.configure('views', { autoescape : true, express : app });
+
+app.set("view engine", "nunjucks");
 app.use(express.static('public'));
 
 app.get('/', function(req, res) {
-    res.json('{ success :  true }');
+    res.render('index.html', {});
+});
+
+app.get('/:product', function(req, res) {
+
 });
 
 app.listen(5000, () => {
