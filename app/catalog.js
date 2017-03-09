30473f228fe299bf878ec2164fd2b2be3f9bf2fc
Paul Gilbertson <paul.gilbertson@gmail.com>
2017-03-09 17:18:51 +0000

    Add date filter

--- app/catalog.js
+++ app/catalog.js
@@ -1,6 +1,7 @@
 var pgp = require('pg-promise')();
 
-var connectionString = `postgres://${process.env.PGCONN}@172.31.17.103:5432/topsat`;
+//var connectionString = `postgres://${process.env.PGCONN}@172.31.17.103:5432/topsat`;
+var connectionString = `postgres://${process.env.PGCONN}@ec2-52-212-152-232.eu-west-1.compute.amazonaws.com:5432/topsat`;
 
 var db = pgp(connectionString);
 
@@ -12,5 +13,13 @@ var getDatasets = function(product) {
     }).catch(error => { console.log(error); });
 }
 
-exports.getDatasets = getDatasets;
+var getDatasetsByDate = function(product, start, end) {
+    return db.many({
+        name : 'getDatasets',
+        text : 'SELECT * FROM chlor_a WHERE product = $1 AND startDate <= $2 AND endDate >= $3 ORDER BY startDate', 
+        values : [product, end.format('YYYY-MM-DD'), start.format('YYYY-MM-DD')]
+    }).catch(error => { console.log(error); });
+}
 
+exports.getDatasets = getDatasets;
+exports.getDatasetsByDate = getDatasetsByDate;
--- app/server.js
+++ app/server.js
@@ -11,9 +11,9 @@ app.set("view engine", "nunjucks");
 app.use(express.static('app/public'));
 
 var products = {
-    monthly : { id : 'monthly', name : 'Chlorophyll-A Monthly', files : 220, catalogProduct : 'monthly' },
-    fiveday : { id : 'fiveday', name : 'Chlorophyll-A 5-day', files : 1341, catalogProduct : '5day' },
-    daily : { id : 'daily', name : 'Chlorophyll-A Daily', files : 6666, catalogProduct : 'daily' }
+    monthly : { id : 'monthly', name : 'Chlorophyll-a Concentration for UK Waters - Monthly', files : 220, catalogProduct : 'monthly' },
+    fiveday : { id : 'fiveday', name : 'Chlorophyll-a Concentration for UK Waters - 5-day', files : 1341, catalogProduct : '5day' },
+    daily : { id : 'daily', name : 'Chlorophyll-a Concentration for UK Waters - Daily', files : 6666, catalogProduct : 'daily' }
 };
 
 app.get('/', function(req, res) {
@@ -22,16 +22,21 @@ app.get('/', function(req, res) {
     });
 });
 
-app.get('/data/:product', function(req, res) {
-    catalog.getDatasets(products[req.params.product].catalogProduct).then(datasets => {
+app.get('/data/:product/:start/:end', function(req, res) {
+    var sm = moment(req.params.start);
+    var em = moment(req.params.end);
+
+    catalog.getDatasetsByDate(products[req.params.product].catalogProduct, sm, em).then(datasets => {
         res.render('data.html', {
             product : products[req.params.product],
             data : datasets,
-            moment : moment
+            moment : moment,
+            start : sm.format('YYYY-MM-DD'),
+            end : em.format('YYYY-MM-DD')
         });
     });
 });
 
 app.listen(process.env.PORT, () => {
-    console.log('Server operational');
+    console.log('Server operational on port ' + process.env.PORT);
 });
